9b5c753a5dca5012c8e4b9a02b69285261fcea7a
Leo <zhk20042004@gmail.com>
2026-03-26 17:12:46 -0400

    Update shellmemory.h adding macros

--- src/shellmemory.h
+++ src/shellmemory.h
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+#ifndef FRAME_STORE_SIZE
+#define FRAME_STORE_SIZE 18
+#endif
+#ifndef VAR_MEM_SIZE
+#define VAR_MEM_SIZE 10
+#endif
+
 #define FRAME_SIZE 3
 
 //variable store
@@ -13,4 +21,4 @@ void frame_store_set_line(int frame, int line_in_frame, const char *text);
 const char *frame_store_get_line(int frame, int line_in_frame);
 void frame_store_free_frame(int frame);
 int frame_store_lru_victim(void);
-void frame_store_print_frame(int_frame);
+void frame_store_print_frame(int frame);
