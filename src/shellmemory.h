244041e9596d606f8b29678e4ff0607e08ac80ea
Leo <zhk20042004@gmail.com>
2026-03-26 17:37:10 -0400

    Update shellmemory.h

--- src/shellmemory.h
+++ src/shellmemory.h
@@ -22,5 +22,6 @@ const char *frame_store_get_line(int frame, int line_in_frame);
 void frame_store_free_frame(int frame);
 int frame_store_lru_victim(void);
 void frame_store_print_frame(int frame);
-void frame_store_is_allocated();
+void frame_store_is_allocated(int frame);
 void frame_store_num_frames();
+void frame_store_mark_used(int frame)
