7611c7fc17208ffa2df8f74dbe74c17c7d66f2ba
ARSD-DOGE <billzhuanga5342@gmail.com>
2026-04-02 20:51:08 -0400

    Enhance shell memory with frame store functions
    Added frame store functionality and updated memory definitions.

--- assignment 3/shellmemory.h
+++ assignment 3/shellmemory.h
@@ -3,9 +3,20 @@
 
 #define MEM_SIZE 1000
 #define MAX_LOADED_SCRIPTS 5
+#define FRAME_SIZE 3
+
+#ifndef FRAME_STORE_SIZE
+#define FRAME_STORE_SIZE 18
+#endif
+#ifndef VAR_MEM_SIZE
+#define VAR_MEM_SIZE 10
+#endif
+
+#define FRAME_SIZE 3
 
 extern loaded_script loaded_scripts[MAX_LOADED_SCRIPTS];
 extern int num_loaded_scripts;
+
 
 void mem_init();
 char *mem_get_value(char *var);
@@ -15,5 +26,17 @@ char * mem_get_line(int index);
 void mem_cleanup_script(int start_index, int end_index);
 int mem_load_script(char *script, int *start_index);
 int mem_load_from_batch(int *start_index);
+
+//frame store
+void frame_store_init();
+int frame_store_alloc_frame();
+void frame_store_set_line(int frame, int line_in_frame, const char *text);
+const char *frame_store_get_line(int frame, int line_in_frame);
+void frame_store_free_frame(int frame);
+int frame_store_lru_victim(void);
+void frame_store_print_frame(int frame);
+int frame_store_is_allocated(int frame);
+int frame_store_num_frames();
+void frame_store_mark_used(int frame);
 
 #endif
