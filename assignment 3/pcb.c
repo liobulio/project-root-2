1eb6786e11457320f14ef0d59c877449b6d9ebd1
ARSD-DOGE <billzhuanga5342@gmail.com>
2026-04-02 22:00:10 -0400

    Enhance PCB structure initialization
    Refactor PCB creation to initialize additional fields and correct formatting.

--- assignment 3/pcb.c
+++ assignment 3/pcb.c
@@ -5,15 +5,22 @@ PCB * make_pcb(int pid, int start, int length, char *script_name) {
     PCB * new_pcb = malloc(sizeof(PCB));
 
     if (new_pcb == NULL){
-      return NULL;
+        return NULL;
     }
-  
+
     new_pcb->PID = pid;
-    new_pcb->start_position= start;
+    new_pcb->start_position = start;
     new_pcb->length = length;
-    new_pcb->pc_instruction_index = start;
-    new_pcb->job_length_score = length; 
-    new_pcb->next = NULL;
+    new_pcb->pc_instruction_index = 0;
+    new_pcb->job_length_score = length;
+
     new_pcb->script_name = strdup(script_name);
+
+    new_pcb->page_table = NULL;
+    new_pcb->num_pages = 0;
+    new_pcb->total_instructions = length;
+
+    new_pcb->next = NULL;
+
     return new_pcb;
 }
