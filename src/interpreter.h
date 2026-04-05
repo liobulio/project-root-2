77fe312b0ae48f503d23b74ee0c11e88efdce338
Leo <zhk20042004@gmail.com>
2026-04-04 22:37:42 -0400

    Delete src directory

--- src/interpreter.h
+++
@@ -1,6 +0,0 @@
-#include <stddef.h>
-int interpreter(char *command_args[], int args_size);
-int help();
-
-struct PCB *run_pcb_to_completion(struct PCB *pcb);
-struct PCB *run_pcb_for_n_steps(struct PCB *pcb, size_t n);
